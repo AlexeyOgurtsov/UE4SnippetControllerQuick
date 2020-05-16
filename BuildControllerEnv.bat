10c66dad38e52e07226eb721feba31a3ea83a69f
unknown <alexey.ue4@gmail.com>
2020-05-16 15:14:43 +0300

    To UE 4.25

--- BuildControllerEnv.bat
+++
@@ -1,8 +0,0 @@
-C:
-start /B SetTarg.bat
-set location=C:\U4L\Snip\Controller
-set projectname=ProjControllerQuick
-set projectdirname=ProjControllerQuick
-set configpostfix=Editor
-set sniploc=C:\U4L\Snip
-call %sniploc%\MyBuildEnv.bat %projectname%%configpostfix% %location%\%projectdirname%\%projectname%
